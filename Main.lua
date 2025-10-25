eae5da66cc34612a940434db31fc8b62788d196f
SidKosmo <Antonenkovevgen@mail.ru>
2025-10-25 21:59:23 +0300

    Склад

--- Main.lua
+++ Main.lua
@@ -1,7 +1,33 @@
-print("Hello! I'm work!")
-local bridge = peripheral.find("meBridge")
-if bridge then
-  print("ME Bridge good!")
-else
-  print("ME Bridge bad")
+local mon = peripheral.find("monitor") -- Автоматически находит монитор
+local meBridge = peripheral.find("meBridge") -- Автоматически находит мост
+
+if not mon or not meBridge then
+    print("Mon or Bridge bad!")
+    return
+end
+
+mon.setTextScale(0.5) -- Уменьшаем масштаб текста, чтобы влезло больше
+mon.clear()
+
+while true do
+    mon.setCursorPos(1, 1)
+    mon.write("=== Sklad reprt ===")
+
+    local importantItems = {
+        "minecraft:iron_ingot",
+        "minecraft:gold_ingot",
+        "minecraft:diamond",
+        "mekanism:osmium_ingot" -- Пример предмета из другого мода
+    }
+
+    local yPos = 3
+    for i, itemName in ipairs(importantItems) do
+        local item = meBridge.getItem(itemName)
+        local count = item and item.amount or 0
+        mon.setCursorPos(2, yPos)
+        mon.write(string.sub(itemName, 11) .. ": " .. count) -- Обрезаем "minecraft:"
+        yPos = yPos + 1
+    end
+
+    os.sleep(5) -- Обновлять каждые 5 секунд
 end
