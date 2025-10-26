3620dd7e30308a61da71ff75b9eb888757ab60ea
SidKosmo <Antonenkovevgen@mail.ru>
2025-10-26 22:57:48 +0300

    777

--- Main.lua
+++ Main.lua
@@ -1,15 +1,36 @@
-local bridge = peripheral.find("meBridge")
+-- Поставь компьютера между сундуками
+-- Подключи модемы к сундукам
 
-if not bridge then
-    print("ME Bridge not found!")
+local chestFrom = peripheral.wrap("left")   -- Левый сундук
+local chestTo = peripheral.wrap("right")    -- Правый сундук
+
+if not chestFrom or not chestTo then
+    print("Check chest connections!")
     return
 end
 
-print("ME Bridge OK!")
-
--- Just list available methods to see what we can use
-local methods = peripheral.getMethods("meBridge")
-print("Available methods:")
-for i, method in ipairs(methods) do
-    print("  " .. method)
+function transferItems()
+    local itemsMoved = 0
+    
+    -- Получаем список предметов в исходном сундуке
+    local items = chestFrom.list()
+    
+    for slot, item in pairs(items) do
+        if item then
+            -- Переносим предмет из слота в слота
+            local itemStack = chestFrom.getItemDetail(slot)
+            if itemStack then
+                -- Пытаемся добавить в целевой сундук
+                local remaining = chestTo.pushItems(peripheral.getName(chestFrom), slot, itemStack.count)
+                if remaining < itemStack.count then
+                    itemsMoved = itemsMoved + (itemStack.count - remaining)
+                    print("Moved " .. (itemStack.count - remaining) .. " " .. itemStack.name)
+                end
+            end
+        end
+    end
+    
+    return itemsMoved
 end
+
+print("Total moved: " .. transferItems())
