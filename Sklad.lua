5d575fc01011f752cbf02c35e6ebe2b52bc5a901
SidKosmo <Antonenkovevgen@mail.ru>
2025-10-26 23:36:53 +0300

    10.2

--- Sklad.lua
+++ Sklad.lua
@@ -1,33 +1,24 @@
 local left = peripheral.wrap("left")
 local right = peripheral.wrap("right")
 
-if not left or not right then
-    print("Chests not connected!")
-    return
-end
-
-function continuousTransfer()
-    while true do
-        local moved = 0
-        local fromName = peripheral.getName(left)
-        local items = left.list()
-        
-        for slot, item in pairs(items) do
-            if item then
-                local movedCount = right.pushItems(fromName, slot, item.count)
-                if movedCount > 0 then
-                    print("[" .. os.time() .. "] Moved " .. movedCount .. " " .. item.name)
-                    moved = moved + movedCount
-                end
-            end
-        end
-        
-        if moved == 0 then
-            print("[" .. os.time() .. "] No items to transfer")
-        end
-        
-        os.sleep(5) -- Проверяем каждые 5 секунд
+if not left then
+    print("Left chest not found!")
+else
+    print("Left chest: " .. peripheral.getName(left))
+    print("Available methods:")
+    local methods = peripheral.getMethods("left")
+    for i, method in ipairs(methods) do
+        print("  " .. method)
     end
 end
 
-continuousTransfer()
+if not right then
+    print("Right chest not found!")
+else
+    print("Right chest: " .. peripheral.getName(right))
+    print("Available methods:")
+    local methods = peripheral.getMethods("right")
+    for i, method in ipairs(methods) do
+        print("  " .. method)
+    end
+end
